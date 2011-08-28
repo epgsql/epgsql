1f6b286ae7e1a14631711442b634d4918b617ff5
Anton Lebedevich <mabrek@gmail.com>
2011-08-28 13:37:16 +0400

    decoder skeleton

---
+++ src/pgsql_decoder.erl
@@ -0,0 +1,16 @@
+-module(pgsql_decoder).
+
+-export([init/1,
+         decode/2,
+         encode/2]).
+
+-record(state, {}).
+
+init(Options) ->
+    #state{}.
+
+decode(Data, State = #state{}) ->
+    {ok, State}.
+
+encode(Data, State = #state{}) ->
+    {Data, State}.
