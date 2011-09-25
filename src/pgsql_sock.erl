edce3fad742fd1eefb9a7ffffa7621f9fc3932f8
Anton Lebedevich <mabrek@gmail.com>
2011-09-25 11:42:49 +0400

    initial parameters

--- src/pgsql_sock.erl
+++ src/pgsql_sock.erl
@@ -24,7 +24,7 @@
                 async,
                 ready,
                 timeout,
-                parameters,
+                parameters = [],
                 txstatus}).
 
 %% -- client interface --
