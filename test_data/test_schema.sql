705746087bea9dde968632c7306b4733a156ea65
David N. Welton <davidw@dedasys.com>
2014-12-19 14:11:39 +0100

    Grant permission on schema_version table

--- test_data/test_schema.sql
+++ test_data/test_schema.sql
@@ -87,6 +87,7 @@ begin
 end
 $$ language plpgsql;
 
+GRANT ALL ON TABLE schema_version TO epgsql_test;
 GRANT ALL ON TABLE test_table1 TO epgsql_test;
 GRANT ALL ON TABLE test_table2 TO epgsql_test;
 GRANT ALL ON FUNCTION insert_test1(integer, text) TO epgsql_test;
