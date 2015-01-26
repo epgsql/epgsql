0227504a6adcbbaaf9ac44d4daf0d6c3db06187f
David N. Welton <davidw@dedasys.com>
2015-01-26 11:42:27 +0100

    We no longer need to fiddle with the system pg_hba.conf

--- test_data/test_schema.sql
+++ test_data/test_schema.sql
@@ -4,19 +4,6 @@
 -- this script should be run as the same user the tests will be run as,
 -- so that the test for connecting as the 'current user' succeeds
 --
--- the following lines must be added to pg_hba.conf for all tests to
--- succeed, with $USER replaced by your username, or the user you will
--- run the tests with.
---
--- host    epgsql_test_db1 $USER                   127.0.0.1/32    trust
--- host    epgsql_test_db1 epgsql_test             127.0.0.1/32    trust
--- host    epgsql_test_db1 epgsql_test_md5         127.0.0.1/32    md5
--- host    epgsql_test_db1 epgsql_test_cleartext   127.0.0.1/32    password
--- hostssl epgsql_test_db1 epgsql_test_cert        127.0.0.1/32    cert
---
--- any 'trust all' must be commented out for the invalid password test
--- to succeed.
---
 -- ssl support must be configured, and the sslinfo contrib module
 -- loaded for the ssl tests to succeed.
 
