74c776ce4b29fd0879a9f95c26f6ce5bece9c4ac
alisdair sullivan <alisdair.sullivan@askuity.com>
2015-07-19 14:09:05 -0700

    use postgres 9.4 for tests

--- setup_test_db.sh
+++ setup_test_db.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ -z $(which initdb) ] ; then
-    echo "Postgres not found, you may need to launch like so: PATH=\$PATH:/usr/lib/postgresql/9.3/bin/ $0"
+    echo "Postgres not found, you may need to launch like so: PATH=\$PATH:/usr/lib/postgresql/9.4/bin/ $0"
     exit 1
 fi
 
