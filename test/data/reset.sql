c79b4f37f29fc92e1b5939273bb690574edb8dc0
Сергей Прохоров <seriy.pr@gmail.com>
2017-11-03 14:56:51 +0100

    Fix reset.sql. Fixes #144

--- test/data/reset.sql
+++ test/data/reset.sql
@@ -1,7 +1,8 @@
+DROP DATABASE epgsql_test_db1;
+DROP DATABASE epgsql_test_db2;
+
 DROP USER epgsql_test;
 DROP USER epgsql_test_md5;
 DROP USER epgsql_test_cleartext;
 DROP USER epgsql_test_cert;
-
-DROP DATABASE epgsql_test_db1;
-DROP DATABASE epgsql_test_db2;
+DROP USER epgsql_test_replication;
